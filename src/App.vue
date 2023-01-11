5a99fc79ca9f106f3354e0577926fba55c29787e
CUOR125822 <h15732964521@gmail.com>
2023-01-11 12:36:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="nav">
+    <HelloWorld msg="You did it!" />
+    <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+  </div>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.nav {
+  width: 100%;
+  height: 64px;
+  border: 1px solid black;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <h1 class="green">页面传递数据{{ msg }}</h1>
+</template>
+
+<style scoped>
+
+</style>
